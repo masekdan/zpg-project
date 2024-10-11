9748a2865816d4b4038aae66bf17ef7e014f6681
Daniel Mašek <masekdan03@seznam.cz>
2024-10-11 21:52:03 +0200

    Adjusted tree

--- src/Model.cpp
+++ src/Model.cpp
@@ -22,8 +22,8 @@ void Model::drawModel(float angle)
 	glm::mat4 model = glm::mat4(1.0f);
 
 	model = glm::rotate(model,glm::radians(angle),glm::vec3(0.0f,1.0f,0.0f));
-	model = glm::translate(model,glm::vec3(0.0f,-0.5f,0.0f));
-	model = glm::scale(model,glm::vec3(0.5f));
+	model = glm::translate(model,glm::vec3(0.0f,-0.8f,0.0f));
+	model = glm::scale(model,glm::vec3(0.20f));
 
 	int modelLoc = this->shader->GetLocation("model");
 	glUniformMatrix4fv(modelLoc,1,GL_FALSE,glm::value_ptr(model));
